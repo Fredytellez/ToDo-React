2b44ecc339c92b276fbc9a39db08af09ed50dff9
Fredy Pena <tredy@outlook.com>
2024-01-01 08:29:52 -0500

    ToDo react completo-falta personalizar

---
+++ first-proyect/src/Components/TodoItems.jsx
@@ -0,0 +1,57 @@
+import "./Css/TodoItem.css";
+import tick from "./Assets/tick.png";
+import not_tick from "./Assets/not_tick.png";
+import cross from "./Assets/cross.png";
+import PropTypes from 'prop-types'
+
+function TodoItems({ no, display, text, setTodos }) {
+
+  const deleteTodo = (no)=>{
+    let data = JSON.parse(localStorage.getItem("todos"));
+    data = data.filter((todo)=>todo.no !== no)
+    setTodos(data);
+  }  
+
+  const toggle = (no) => {
+    let data = JSON.parse(localStorage.getItem("todos"));
+    for (let i = 0; i < data.length; i++) {
+      if (data[i].no === no) {
+        if (data[i].display === "") {
+          data[i].display = "line-through";
+        } else {
+          data[i].display = "";
+        }
+        break;
+      }
+    }
+    setTodos(data);
+  };
+
+  return (
+    <div className="todoItems">
+      <div
+        className={`todoitems-container ${display}` }
+        onClick={() => {
+          toggle(no);
+        }}
+      >
+        {display === "" ? (
+          <img src={not_tick} alt="" />
+        ) : (
+          <img src={tick} alt="" />
+        )}
+        <div className="todoItems-text">{text}</div>
+      </div>
+      <img className="todoItems-cross-icon" onClick={()=>{deleteTodo(no)}} src={cross} alt="" />
+    </div>
+  );
+}
+
+TodoItems.propTypes = {
+    no: PropTypes.number.isRequired,
+    display: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    setTodos: PropTypes.func.isRequired,
+  };
+
+export default TodoItems;
